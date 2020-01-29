d8738adf739d3ed2b44ef4e0cd8499971dcfbcac
bsimond <brice.simond@le-campus-numerique.fr>
2020-01-29 16:17:56 +0100

    il faut que tu fasse une boucle pour terminer le jeux

--- action.py
+++ action.py
@@ -1,29 +1,53 @@
 from characters import Wizard
 from characters import Archer
 from characters import Warrior
+from characters import Elfewizard
+from characters import Elfearcher
+from characters import Elfewarrior
+from characters import Dwarfwizard
+from characters import Dwarfarcher
+from characters import Dwarfwarrior
+
 
 def main():
-    merlin = Wizard ("Merlin")
-    oubien = Archer ( "oubien")
-    bourrin= Warrior ( "Bourrin")
+    merlin = Elfewizard ("merlin")
+    miniwizard= Dwarfwizard ( "miniwizard")
+    oubien = Elfearcher ( "oubien")
+    minarcher=Dwarfarcher("minarcher")
+    bourrin= Dwarfwarrior ( "Bourrin")
+    grosbourrin=Elfewarrior("grosbourrin")
+
+    while merlin._life_point or miniwizard._life_point or oubien._life_point or minarcher._life_point or bourrin._life_point or grosbourrin._life_point >=0:
+
+            character_attack = input("qui attaque :")
+            if character_attack == "merlin":
+                weapon, attack_points = merlin.attack()
+            if character_attack == "oubien":
+                weapon , attack_points = oubien.attack()
+            if character_attack== "bourrin":
+                weapon , attack_points = bourrin.attack()
+            if character_attack == "miniwizard":
+                 weapon , attack_points= miniwizard.attack()
+            if character_attack== "minarcher":
+                weapon , attack_points= minarcher.attack()
+            if character_attack == "grosbourrin":
+                weapon , attack_points = grosbourrin.attack()
 
 
-    character_attack = input("qui attaque :")
-    if character_attack == "merlin":
-        weapon, attack_points = merlin.attack()
-    if character_attack == "oubien":
-        weapon , attack_points = oubien.attack()
-    if character_attack== "bourrin":
-        weapon , attack_points = bourrin.attack()
 
-    character_defend = input ( " qui defend :")
-    if character_defend == "merlin":
-        merlin.defend(weapon, attack_points)
-    if character_defend == "oubien":
-        oubien.defend ( weapon, attack_points)
-    if character_defend == "bourrin":
-        bourrin.defend( weapon,attack_points)
-
+            character_defend = input ( " qui defend :")
+            if character_defend == "merlin":
+                merlin.defend (weapon, attack_points)
+            if character_defend == "oubien":
+                oubien.defend ( weapon, attack_points)
+            if character_defend == "bourrin":
+                bourrin.defend( weapon,attack_points)
+            if character_defend == "miniwizard":
+                miniwizard.defend ( weapon,attack_points)
+            if character_defend == "minarcher":
+                minarcher.defend ( weapon,attack_points)
+            if character_defend == "grosbourrin":
+                grosbourrin.defend (weapon,attack_points)
 
 if __name__ == '__main__':
      main()
--- characters.py
+++ characters.py
@@ -2,13 +2,23 @@ from random import randint
 
 
 class Character:
-    max_life_point = 12
+    _max_life_point = 12
+    bowbonus=0
+    swordbonus=0
 
     def __init__(self, name):
         self.name = name
-        self.life_point = self.max_life_point
-        self.height = randint(170, 190)
-        self.weight = randint(70, 90)
+        self._life_point =self._max_life_point
+        self._height=randint(170,190)
+        self._weight = randint(70, 90)
+    def _get_height (self):
+        return self._height
+    def _get_weight(self):
+        return self._weight
+
+
+    height = property( _get_height)
+    weight = property(_get_weight)
 
     def dices_roll(self):
         magic = randint(1, self.magicdice)
@@ -20,9 +30,18 @@ class Character:
     def attack(self):
         dices_roll = self.dices_roll()
         winnerdices = sorted(dices_roll, key=lambda x: x[1], reverse=True)
-        results = winnerdices[0]
-        print(str(self.name) + " utilise " + str(results))
-        return results
+        weapon, attack_points= winnerdices[0]
+        if weapon == "sword":
+            attack_points = attack_points + self.swordbonus
+            print(str(self.name) + " utilise " + str(weapon)+" et fait un " + str(attack_points))
+            return weapon , attack_points
+        if weapon == " bow":
+            attack_points= attack_points +self.bowbonus
+            print(str(self.name) + " utilise " + str(weapon)+" et fait un " + str(attack_points))
+            return weapon, attack_points
+        else :
+            print(str(self.name) + " utilise " + str(weapon)+" et fait un " + str(attack_points))
+            return weapon, attack_points
 
     def defend(self, weapon, attack_point, ):
         dices_roll = dict(self.dices_roll())
@@ -31,13 +50,13 @@ class Character:
         print(str(self.name) + " se defend en faisant " + str(defendweapon))
 
         if attack_point > defendweapon:
-            self.life_point = self.life_point - difference
-            print("il reste  à " + str(self.name) + " " + str(self.life_point) + "  point de vie")
-            return self.life_point
+            self._life_point = self._life_point - difference
+            print("il reste  à " + str(self.name) + " " + str(self._life_point) + "  point de vie")
+            return self._life_point
         if attack_point <= defendweapon:
-            self.life_point = self.life_point
-            print(str(self.name) + " a toujours " + str(self.life_point))
-            return self.life_point
+            self._life_point = self._life_point
+            print(str(self.name) + " a toujours " + str(self._life_point))
+            return self._life_point
 
 
 class Wizard(Character):
@@ -46,23 +65,22 @@ class Wizard(Character):
     bowdice = 10
 
     def attack(self):
-
-        weapon, attack_point = super().attack()
+        weapon, attack_points = super().attack()
         magicdammage = randint(1, self.magicdice)
-
-        if attack_point < magicdammage:
+        if attack_points < magicdammage:
             return "magic", magicdammage
         elif weapon == "sword":
-            bonussword = (self.height + self.weight)// 40
-            totalsword = bonussword + attack_point
+
+            bonussword = (self._height + self._weight)// 40
+            totalsword = bonussword + attack_points
             print(totalsword)
             return "sword", totalsword
         elif weapon == "bow":
-            bonusbow = (self.height - 170) % 3
-            totalbow = bonusbow + attack_point
+            bonusbow = (self._height - 170) % 3
+            totalbow = bonusbow + attack_points
             print(totalbow)
             return "bow", totalbow
-        return weapon ,attack_point
+        return weapon ,attack_points
 
 
 class Archer(Character):
@@ -71,38 +89,69 @@ class Archer(Character):
     bowdice = 10
 
     def attack(self):
-        weapon, attack_point = super().attack()
+        weapon, attack_points = super().attack()
         if weapon == "magic":
-           bonusmagic= self.weight//20
-           totalmagic= attack_point + bonusmagic + 1
+           bonusmagic= self._weight//20
+           totalmagic= attack_points + bonusmagic + 1
            print(totalmagic)
            return "magic" , totalmagic
         elif weapon == "sword":
-            bonussword= self.height//40
-            totalsword = attack_point + bonussword +1
+            bonussword= self._height//40
+            totalsword = attack_points + bonussword +1
             print(totalsword)
             return "sword", totalsword
-        return weapon , attack_point
+        return weapon , attack_points
 
 
 class Warrior(Character):
 
-    max_life_point = 16
+    _max_life_point = 16
     magicdice = 8
     sworddice = 12
     bowdice = 10
 
 
     def attack(self):
-        weapon, attack_point = super().attack()
+        weapon, attack_points = super().attack()
         if weapon == "magic":
-            bonusmagic=self.weight//30
-            totalmagic= bonusmagic + attack_point
+            bonusmagic=self._weight//30
+            totalmagic= bonusmagic + attack_points
             print(totalmagic)
             return "magic",totalmagic
         elif weapon=="bow":
-            bonusbow= (self.height -170)%3
-            totalbow= bonusbow + attack_point
+            bonusbow= (self._height -170)%3
+            totalbow= bonusbow + attack_points
             print(totalbow)
             return "bow",totalbow
-        return weapon ,attack_point
+        return weapon ,attack_points
+
+
+class Dwarf:
+
+    swordbonus=2
+
+
+
+class Elfe:
+
+    bowbonus=2
+
+
+class Dwarfwizard ( Dwarf , Wizard):
+    pass
+
+class Dwarfarcher ( Dwarf , Archer):
+    pass
+
+class Dwarfwarrior ( Dwarf , Warrior):
+    pass
+
+class Elfewizard ( Elfe, Wizard) :
+    pass
+
+class Elfearcher ( Elfe , Archer):
+    pass
+
+class Elfewarrior (Elfe , Warrior) :
+    pass
+
